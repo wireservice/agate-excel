3db3b8eb6f0e87a752d6f4738d134fca87a10083
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-12-15 13:38:41 -0500

    build: Add 3.13 3.14 support. Drop 3.8 3.9 support.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 project = 'agate-excel'
 copyright = '2017, Christopher Groskopf'
-version = '0.4.1'
+version = '0.4.2'
 release = version
 
 # -- General configuration ---------------------------------------------------
